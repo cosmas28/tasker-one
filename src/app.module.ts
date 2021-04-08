71026a535c04127f2ff4f18b4bb998380ff84066
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-04-08 14:07:40 +0300

    remove unwanted controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CommonController } from './tasker-one/common/common.controller';
 import { TaskerController } from './tasker/tasker.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CommonController, TaskerController],
+  controllers: [AppController, TaskerController],
   providers: [AppService],
 })
 export class AppModule {}
