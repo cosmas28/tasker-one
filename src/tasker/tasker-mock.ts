e5cb2319b900ed95ca6e778e1d48c7ad79c8f403
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-04-08 14:02:45 +0300

    add tasks mock data

---
+++ src/tasker/tasker-mock.ts
@@ -0,0 +1,29 @@
+import {TaskerDTO} from './tasker.dto';
+
+export const tasks: TaskerDTO[] = [
+	{
+		id: '1',
+		title: 'TRA errands',
+		status: 'completed',
+	},
+	{
+		id: '2',
+		title: 'Business checkin',
+		status: 'backlog',
+	},
+	{
+		id: '3',
+		title: 'Grocery shopping',
+		status: 'backlog',
+	},
+	{
+		id: '4',
+		title: 'Slang work',
+		status: 'backlog',
+	},
+	{
+		id: '5',
+		title: 'Reading mails',
+		status: 'backlog',
+	},
+];
