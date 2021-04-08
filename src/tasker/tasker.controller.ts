94b04995f3e6a4a32ad6859ab69b5384ea8ab288
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-04-08 14:46:17 +0300

    create a POST tasks endpoint

--- src/tasker/tasker.controller.ts
+++ src/tasker/tasker.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post} from '@nestjs/common';
 
 import {TaskerDTO} from './tasker.dto';
 
-import {tasks as tasksData} from './tasker-mock';
+import {tasks} from './tasker-mock';
+
+let tasksData = tasks;
  
 @Controller('tasker')
 export class TaskerController {
 	@Get('tasks')
 	getTasks(): TaskerDTO[] {
 		return tasksData;
+	}
+
+	@Post('tasks')
+	createTask(@Body() createTask: TaskerDTO): TaskerDTO {
+		const newTask: TaskerDTO = {
+			id: (tasksData.length + 1).toString(),
+			...createTask,
+		};
+
+		tasksData = [...tasksData, newTask];
+
+		return newTask;
 	}
 }
